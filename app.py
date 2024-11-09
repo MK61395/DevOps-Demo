de91bec582351a788e2721051778db26e1b9dfec
Muhammad Kashif <91327646+MK61395@users.noreply.github.com>
2024-11-09 21:12:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 
+print(f"DB_HOST: {os.getenv('DB_HOST')}")
+print(f"DB_NAME: {os.getenv('DB_NAME')}")
+print(f"DB_USER: {os.getenv('DB_USER')}")
+
 # Connect to the RDS instance
 conn = psycopg2.connect(
     host=os.getenv('DB_HOST'),  # RDS instance endpoint
